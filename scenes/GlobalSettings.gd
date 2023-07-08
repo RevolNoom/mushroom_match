a908471ab2f5e89e935a6b93c454595d5b1c9562
Rev <revolnoom7801@gmail.com>
2023-07-08 21:58:51 +0700

    --amend

--- scenes/GlobalSettings.gd
+++ scenes/GlobalSettings.gd
@@ -1,16 +1,37 @@
 extends Node
 
+var config_filepath = "user://player.cfg"
+var configfile = ConfigFile.new()
 
-func _ready():
-	#$Music.play()
-	pass
 
-var sfx_enabled = true
-func EnableSfx(enable: bool):
-	sfx_enabled = enable 
+func _enter_tree():
+	if configfile.load(config_filepath) != OK:
+		configfile.set_value("volume", "sfx", 0)
+		configfile.set_value("volume", "sfx_enabled", true)
+		configfile.set_value("volume", "music", 0)
+		configfile.set_value("volume", "music_enabled", true)
+		configfile.set_value("player", "highscore", 0)
+		configfile.save(config_filepath)
+
+
+func get_value(config_section: String, key_name: String) -> Variant:
+	return configfile.get_value(config_section, key_name)
+	
+	
+func set_value(config_section: String, key_name: String, value: Variant):
+	#print("Setting " + config_section + " " + key_name + ": " + str(value))
+	configfile.set_value(config_section, key_name, value)
+	$ResaveTimer.start()
+
+
+@export var sfx_enabled = true:
+	set(enable):
+		configfile.set_value("volume", "sfx_enabled", sfx_enabled)
+		sfx_enabled = enable 
 
 
 func SetSfxVolume(vol_db: float):
+	configfile.set_value("volume", "sfx", vol_db)
 	for sfx in $Sfx.get_children():
 		sfx.volume_db = vol_db
 
@@ -18,3 +39,7 @@ func SetSfxVolume(vol_db: float):
 func PlaySfx(sfx_name: String):
 	if sfx_enabled:
 		$Sfx.get_node(sfx_name).play()
+
+
+func _on_resave_timer_timeout():
+	configfile.save(config_filepath)
